c74bdffc761f6c00c552d65f8e3542071780e378
Ethan De La Rosa <dlrethan10@gmail.com>
2022-10-01 23:21:32 -0700

    added sprites

--- assets/index.js
+++ assets/index.js
@@ -12,7 +12,16 @@ const background = new Sprite({
     x: 0,
     y: 0,
   },
-  imageSrc: "./imgs/background.png",
+  imageSrc: "./assets/imgs/background.png",
+});
+const shop = new Sprite({
+  position: {
+    x: 625,
+    y: 128,
+  },
+  imageSrc: "./assets/imgs/shop.png",
+  scale: 2.75,
+  framesMax: 6,
 });
 
 const user = new Fighter({
@@ -27,6 +36,43 @@ const user = new Fighter({
   offset: {
     x: 0,
     y: 0,
+  },
+  imageSrc: "./assets/imgs/samuraiMack/Idle.png",
+  framesMax: 8,
+  scale: 2,
+  offset: {
+    x: 100,
+    y: 95,
+  },
+  sprites: {
+    idle: {
+      imageSrc: "./assets/imgs/samuraiMack/Idle.png",
+      framesMax: 8,
+    },
+    run: {
+      imageSrc: "./assets/imgs/samuraiMack/Run.png",
+      framesMax: 8,
+    },
+    jump: {
+      imageSrc: "./assets/imgs/samuraiMack/Jump.png",
+      framesMax: 2,
+    },
+    fall: {
+      imageSrc: "./assets/imgs/samuraiMack/Fall.png",
+      framesMax: 2,
+    },
+    attack1: {
+      imageSrc: "./assets/imgs/samuraiMack/Attack1.png",
+      framesMax: 6,
+    },
+  },
+  attackBox: {
+    offset: {
+      x: 150,
+      y: 50,
+    },
+    width: 150,
+    height: 50,
   },
 });
 
@@ -43,6 +89,43 @@ const enemy = new Fighter({
   offset: {
     x: -50,
     y: 0,
+  },
+  imageSrc: "./assets/imgs/kenji/Idle.png",
+  framesMax: 4,
+  scale: 2,
+  offset: {
+    x: 100,
+    y: 105,
+  },
+  sprites: {
+    idle: {
+      imageSrc: "./assets/imgs/kenji/Idle.png",
+      framesMax: 4,
+    },
+    run: {
+      imageSrc: "./assets/imgs/kenji/Run.png",
+      framesMax: 8,
+    },
+    jump: {
+      imageSrc: "./assets/imgs/kenji/Jump.png",
+      framesMax: 2,
+    },
+    fall: {
+      imageSrc: "./assets/imgs/kenji/Fall.png",
+      framesMax: 2,
+    },
+    attack1: {
+      imageSrc: "./assets/imgs/kenji/Attack1.png",
+      framesMax: 4,
+    },
+  },
+  attackBox: {
+    offset: {
+      x: -70,
+      y: 50,
+    },
+    width: 150,
+    height: 50,
   },
 });
 
@@ -66,40 +149,7 @@ const keys = {
     pressed: false,
   },
 };
-function rectangularCollision({ rectangle1, rectangle2 }) {
-  return (
-    rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
-      rectangle2.position.x &&
-    rectangle1.attackBox.position.x <=
-      rectangle2.position.x + rectangle2.width &&
-    rectangle1.attackBox.position.y + rectangle1.attackBox.height >=
-      rectangle2.position.y &&
-    rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
-  );
-}
-function determineWinner({ user, enemy, timerId }) {
-  clearTimeout(timerId);
-  document.querySelector("#displayText").style.display = "flex";
-  if (user.health === enemy.health) {
-    document.querySelector("#displayText").innerHTML = "Tie";
-  } else if (user.health > enemy.health) {
-    document.querySelector("#displayText").innerHTML = "Player 1 Wins!";
-  } else {
-    document.querySelector("#displayText").innerHTML = "Player 2 Wins!";
-  }
-}
-let timer = 60;
-let timerId;
-function decreaseTimer() {
-  if (timer > 0) {
-    timerId = setTimeout(decreaseTimer, 1000);
-    timer--;
-    document.querySelector("#timer").innerHTML = timer;
-  }
-  if (timer === 0) {
-    determineWinner({ user, enemy });
-  }
-}
+
 decreaseTimer();
 
 function animate() {
@@ -107,22 +157,44 @@ function animate() {
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
   background.update();
+  shop.update();
   user.update();
   enemy.update();
 
   user.velocity.x = 0;
   enemy.velocity.x = 0;
+
   //   Player Movement
+
   if (keys.a.pressed && user.lastKey === "a") {
     user.velocity.x = -3;
+    user.switchSprite("run");
   } else if (keys.d.pressed && user.lastKey === "d") {
     user.velocity.x = 3;
+    user.switchSprite("run");
+  } else {
+    user.switchSprite("idle");
   }
-  // Enemy Movement
+  // Jumping
+  if (user.velocity.y < 0) {
+    user.switchSprite("jump");
+  } else if (user.velocity.y > 0) {
+    user.switchSprite("fall");
+  }
   if (keys.ArrowLeft.pressed && enemy.lastKey === "ArrowLeft") {
+    // Enemy Movement
     enemy.velocity.x = -3;
+    enemy.switchSprite("run");
   } else if (keys.ArrowRight.pressed && enemy.lastKey === "ArrowRight") {
     enemy.velocity.x = 3;
+    enemy.switchSprite("run");
+  } else {
+    enemy.switchSprite("idle");
+  }
+  if (enemy.velocity.y < 0) {
+    enemy.switchSprite("jump");
+  } else if (enemy.velocity.y > 0) {
+    enemy.switchSprite("fall");
   }
   // detect collision
   if (
@@ -130,11 +202,16 @@ function animate() {
       rectangle1: user,
       rectangle2: enemy,
     }) &&
-    user.isAttacking
+    user.isAttacking &&
+    user.framesCurrent === 4
   ) {
     user.isAttacking = false;
     enemy.health -= 20;
     document.querySelector("#enemyHealth").style.width = enemy.health + "%";
+  }
+  //  if player misses
+  if (user.isAttacking && user.framesCurrent === 4) {
+    user.isAttacking = false;
   }
   if (
     rectangularCollision({
@@ -146,6 +223,10 @@ function animate() {
     enemy.isAttacking = false;
     user.health -= 20;
     document.querySelector("#userHealth").style.width = user.health + "%";
+  }
+  //  if enemy misses
+  if (enemy.isAttacking && enemy.framesCurrent === 2) {
+    enemy.isAttacking = false;
   }
   // end game if health = 0
   if (enemy.health <= 0 || user.health <= 0) {
